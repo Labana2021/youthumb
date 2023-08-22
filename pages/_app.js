0053182640c53a2048e46a938447b2e692341ff8
Labana2021 <76840676+Labana2021@users.noreply.github.com>
2023-08-22 21:29:17 +0100

    image download button 2

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/index.css";
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics/dist/types/components/GoogleAnalytics/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -17,16 +18,8 @@ function MyApp({ Component, pageProps }) {
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
-      <script strategy="afterIntercative" src="https://www.googletagmanager.com/gtag/js?id=G-XT1TR5ED66" />
 
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${G - XT1TR5ED66}');
-    `}
-      </Script>
+      <GoogleAnalytics measurementId="G-XT1TR5ED66" />
       <Component {...pageProps} />
     </Fragment>
   );
