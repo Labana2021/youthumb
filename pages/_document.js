5e17b227bf4fad4a501953a292b0bff767bd82a5
Labana2021 <76840676+Labana2021@users.noreply.github.com>
2023-08-22 21:52:16 +0100

    google analytic

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+                 //- Google Tag Manager
+          <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MBF5T3ZX');</script>
+         // End Google Tag Manager
           <GoogleAnalytics measurementId="G-XT1TR5ED66" />
           <meta name="description" content="Your YouTube thumbnail download grabber saver description here" />
           <meta name="keywords" content="YouTube, thumbnail, download, grabber, saver" />
@@ -46,6 +53,10 @@ class MyDocument extends Document {
 
 
           <NextScript />
+         //- Google Tag Manager (noscript)
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBF5T3ZX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          // End Google Tag Manager (noscript)
         </body>
       </Html>
     );
