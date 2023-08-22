c42c116084e965c623d21d19ecd7f06b3b554284
Labana2021 <76840676+Labana2021@users.noreply.github.com>
2023-08-22 12:43:20 +0100

    adding adsence code

--- pages/_document.js
+++ pages/_document.js
@@ -23,9 +23,18 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
+          // Include the AdSense ad unit in the header
+          <div class="header-ad">
+            <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="YOUR_ADSENSE_CLIENT_ID"
+              data-ad-slot="YOUR_ADSENSE_SLOT_ID"
+              data-ad-format="auto"></ins>
+          </div>
           <NextScript />
         </body>
       </Html>
